b7e4ee1433b97628aea34e82d874fd786d3e99a0
Vignesh Suthanthirapandian <vigneshsuth@FWS-CHE-LT-2635.local>
2019-07-04 19:04:06 +0530

    Ruby - Day 1

---
+++ first.rb
@@ -0,0 +1,11 @@
+#print will print in the same line
+print "hello world from print"
+
+#puts will print in the new line
+puts "hello world form puts"
+
+
+print "hello world from print"
+
+
+puts "hello world form puts"
---
+++ third_simple_variables.rb
@@ -0,0 +1,12 @@
+  a =  "Ruby"
+  b = "Python"
+
+  puts ("Welcom to the new programming language " + a)
+  puts ("Welcom to the new programming language " + b + " ,Do well!!!!")
+
+
+  a =  "Aws"
+  b = "Perl"
+
+  puts ("Welcom to the new programming language " + a)
+  puts ("Welcom to the new programming language " + b + " ,Do well!!!!")
