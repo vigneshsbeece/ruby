58f407b5cde3a0763afb5d0149409f28b893ab99
Vignesh Suthanthirapandian <vigneshsuth@FWS-CHE-LT-2635.local>
2019-07-05 19:32:15 +0530

    Day 2

---
+++ datatypes.rb
@@ -0,0 +1,9 @@
+#string
+name = "Govind"
+#number
+age = "75"
+#true or false
+ismale = "true"
+
+
+puts (+ name +" "+ age +" "+ismale)
---
+++ special_characters_and_default_methods.rb
@@ -0,0 +1,22 @@
+
+#Escape the special character using the \ option
+puts "Welcome to \"Ruby\""
+
+
+text="A123456789Z"
+#Prints in upper case
+puts text.upcase()
+#Prints in lower case
+puts text.downcase()
+#Removes the empty space in the beginning
+puts text.strip()
+#Prints the length of the string
+puts text.length()
+#Search for a string and prints True/False
+puts text.include? "1"
+puts text.include? "0"
+#prints the value of the particular index
+puts text[10]
+
+puts "hello".upcase().downcase().length
+()
