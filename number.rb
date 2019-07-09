0698a7d6424bbf3a82ada8b192a0c023a740db05
Vignesh Suthanthirapandian <vigneshsuth@FWS-CHE-LT-2635.local>
2019-07-09 15:35:51 +0530

    Day3

---
+++ number.rb
@@ -0,0 +1,20 @@
+#normal numbers
+a = 10
+puts a
+#Integers
+b = -10
+puts b.abs()
+puts b
+#Floats
+c = 10.12
+puts c
+puts c.round()
+puts c.ceil()
+
+#math operations with numbers
+puts 7 + 8
+puts 7 - 8
+puts 7 * 8
+puts 80 / 8
+puts 81 % 8
+puts 7 ** 2
--- third_simple_variables.rb
+++ third_simple_variables.rb
@@ -10,3 +10,7 @@
 
   puts ("Welcom to the new programming language " + a)
   puts ("Welcom to the new programming language " + b + " ,Do well!!!!")
+
+ puts ("Enter the new text")
+  a = gets.chomp()
+ puts ("Upper Case of variable a is " + a.upcase() )
